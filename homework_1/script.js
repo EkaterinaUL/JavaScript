3c5aae6705ff299dcb4177dfd584ae5c0900b37c
Ekaterina <katt_217@mail.ru>
2023-02-16 23:20:01 +0800

    add JS homework 1

---
+++ homework_1/script.js
@@ -0,0 +1,16 @@
+//Задача 1
+
+let degreeCelsius;
+degreeCelsius = 20;
+let degreeFarengeit = (9/5)*degreeCelsius +32;
+console.log(`Температура в Цельсиях: ${degreeCelsius}`);
+console.log(`Температура в Фарингейтах: ${degreeFarengeit}`);
+
+
+//Задача 2
+
+let firstName;
+firstName = 'Владимир';
+let admin;
+admin = firstName;
+console.log(admin);
